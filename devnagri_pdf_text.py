72405dbe5139d3142c384cf7fc60c007e09ab57b
ujjwalraajput <ujjwal.raajput@gmail.com>
2021-07-27 16:21:53 +0530

    datuh

--- devnagri_pdf_text.py
+++ devnagri_pdf_text.py
@@ -7,17 +7,17 @@ class Font:
         self.aksrnam_id = {s.split('"')[3]: int(s.split('"')[1])  for s in re.findall(r'<GlyphID id=.*/>', vidih)}
         self.aksrnam_unicode = {s.split('"')[3]: chr(int(s.split('"')[1][2:], 16)) for s in re.findall(r'<map code=.*/>', vidih)}
 
-        self.aksrnam_dhatvh = dict()
+        self.aksrnam_datvh = dict()
         for s in re.findall(r'<LigatureSet.*?</LigatureSet>', vidih, re.DOTALL):
             prtmdatuh = s.split('"')[1]
             for t in re.findall(r'<Ligature components=.*/>', s):
-                self.aksrnam_dhatvh[t.split('"')[3]] = [prtmdatuh] + t.split('"')[1].split(',')
+                self.aksrnam_datvh[t.split('"')[3]] = [prtmdatuh] + t.split('"')[1].split(',')
 
         self.adesah = []
         for s in re.findall(r'<Substitution in=.*/>', vidih):
             self.adesah.append((s.split('"')[3].split(','), s.split('"')[1]))
 
-        #print([a for a in self.aksrnam_id.keys() if not a in self.aksrnam_unicode.keys() and not a in self.aksrnam_dhatvh.keys()])
+        #print([a for a in self.aksrnam_id.keys() if not a in self.aksrnam_unicode.keys() and not a in self.aksrnam_datvh.keys()])
 
         #self.pscimah = set([s.split('"')[1] for s in re.findall(r'<psName name=.*/>', vidih)])
 
@@ -41,10 +41,10 @@ class Font:
                 print(aksrnam)
             return self.aksrnam_unicode[aksrnam]
 
-        if aksrnam in self.aksrnam_dhatvh.keys():
+        if aksrnam in self.aksrnam_datvh.keys():
             if prkriya:
-                print('धात॑वः ' + aksrnam + ' > ' + ' + '.join(self.aksrnam_dhatvh[aksrnam]))
-            return self.id_unicode([self.aksrnam_id[datuh] for datuh in self.aksrnam_dhatvh[aksrnam]], prkriya)
+                print('धात॑वः ' + aksrnam + ' > ' + ' + '.join(self.aksrnam_datvh[aksrnam]))
+            return self.id_unicode([self.aksrnam_id[datuh] for datuh in self.aksrnam_datvh[aksrnam]], prkriya)
 
         for adesh in self.adesah:
             if len(adesh[0]) == 1 and adesh[0][0] == aksrnam:
